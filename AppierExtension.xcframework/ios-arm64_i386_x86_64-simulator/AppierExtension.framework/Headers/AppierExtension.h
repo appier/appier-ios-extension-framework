61cb34068eceb577713a905acc8cf2ad9f6a0898
RexAppier <rex.chen@appier.com>
2020-12-09 21:16:47 +0800

    AppierFramework.xcframework and pod - 1.0.0

---
+++ AppierExtension.xcframework/ios-arm64_i386_x86_64-simulator/AppierExtension.framework/Headers/AppierExtension.h
@@ -0,0 +1,20 @@
+//
+//  AppierExtension.h
+//  AppierExtension
+//
+//  Created by chunta on 2020/12/9.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AppierExtension.
+FOUNDATION_EXPORT double AppierExtensionVersionNumber;
+
+//! Project version string for AppierExtension.
+FOUNDATION_EXPORT const unsigned char AppierExtensionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppierExtension/PublicHeader.h>
+
+#import <AppierExtension/QGNotificationSdk.h>
+#import <AppierExtension/QGNotificationService.h>
+#import <AppierExtension/QGNotificationContentViewController.h>
